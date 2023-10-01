9129689ab3c0de91b662768166236acf839fdb0f
debashreepratap <debashreepratap91@outlook.com>
2023-10-01 21:26:01 +0530

    minor changes

--- src/main/java/com/jenkins/demo/DemoApplication.java
+++ src/main/java/com/jenkins/demo/DemoApplication.java
@@ -1,6 +1,6 @@
 package com.jenkins.demo;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
